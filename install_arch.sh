8be0f793abeb69ddd9e19bce48df0870f6c95074
nedask <nedas.grubis@gmail.com>
2026-06-12 23:30:39 +0300

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -1,7 +1,3 @@
-sudo pacman -S python-py-cpuinfo
-sudo pacman -S python-colorama
-sudo pacman -S python-distro
-sudo pacman -S python-psutil
 sudo chmod +x deadfetch
 sudo chmod +x acs.py
 sudo chmod +x osinfo.py
